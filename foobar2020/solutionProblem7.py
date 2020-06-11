790dc6996cd672d2078c26e8d847bfd1ad6149a0
vfpachec <vfpachec@LAPTOP-F31RFEFN>
2020-06-10 20:47:45 -0400

    solution problem 7

---
+++ foobar2020/solutionProblem7.py
@@ -0,0 +1,6 @@
+This one I had solved before 
+    https://github.com/franklinvp/foobar/blob/master/foobar2018/extra_challenge3_absorbing_probabilities.py
+I had got the problem from someone else. However, this time I got to actually test the solution
+against the secret test cases.
+
+It passsed!
